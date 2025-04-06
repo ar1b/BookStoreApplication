726cd75360614e6e42a6ad0e59381abe585094ce
ar1b <arib.kamal99@gmail.com>
2025-04-06 15:28:23 -0400

    added client side

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AppContainer } from './components/StyledComponents';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import BookDetailPage from './pages/BookDetailPage';
+import CartPage from './pages/CartPage';
+import AccountPage from './pages/AccountPage';
+
+const App = () => {
+  return (
+    <Router>
+      <AppContainer>
+        <Header />
+        
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/book/:isbn" element={<BookDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/account" element={<AccountPage />} />
+        </Routes>
+      </AppContainer>
+    </Router>
+  );
+};
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HeaderContainer, Logo, Nav, NavLink } from './StyledComponents';
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <Logo>BookStore</Logo>
+      <Nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/cart">Cart</NavLink>
+        <NavLink to="/account">My Account</NavLink>
+      </Nav>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
